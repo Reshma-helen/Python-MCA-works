4f18054218b78afaab4d9779e4107bacdfcc0c55
Reshma Mohanan <116096969+Reshma-helen@users.noreply.github.com>
2022-11-25 22:27:07 +0530

    Update replacestring.py

--- replacestring.py
+++ replacestring.py
@@ -1,23 +1,3 @@
-'''word1=input("enter")
-char=word1[0]
-word=word1.replace(char,'$')
-word=char+word[1:]
-print("before" ,word1)
-print("after", word)'''
-
-
-'''def change_char(str1):
-
-    char=str1[0]
-    str1=str1.replace(char,'$')
-    str1=char+str1[1:]
-
-    return str1
-
-
-word=input("Enter any word")
-print("Before replacement: ",word)
-print("After replacement: ",change_char(word))'''
 
 word_1=input("Enter any word :")
 
