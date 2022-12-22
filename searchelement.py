5ad29b1bc683f3cc85dff2c69120795b1bd3d502
Reshma Mohanan <116096969+Reshma-helen@users.noreply.github.com>
2022-12-22 15:10:52 +0530

    Create searchelement.py

---
+++ searchelement.py
@@ -0,0 +1,10 @@
+list1=[1,2,3,4,5]
+n=int(input("enter a value:"))
+for i in list1:
+        if i==n:
+            print(n,"is a member of the list")
+            break;
+if i!=n:
+        print("Item not in list")
+    
+    
